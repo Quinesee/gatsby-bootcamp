28c9b254f190bc9c436364c81e38e574ea378977
Joshua Norvelle <joshua.norvelle@gmail.com>
2019-09-10 08:13:55 -0500

    Created About, Blog, and Contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return(
+    <div>
+      <h1>About Me</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet magni modi voluptate voluptates. Accusantium in, iure molestiae natus quibusdam quis veritatis. Aliquid at eos hic illum obcaecati optio sit velit!</p>
+    </div>
+  )
+}
+
+export default AboutPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>888.888.8888 | me@here.com</p>
+    </div>
+  )
+}
+
+export default ContactPage;
