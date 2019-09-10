a7f5481e038bc87370af316f672e6a9d04128738
Joshua Norvelle <joshua.norvelle@gmail.com>
2019-09-10 08:24:10 -0500

    Adding internal and external links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const AboutPage = () => {
   return(
     <div>
       <h1>About Me</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet magni modi voluptate voluptates. Accusantium in, iure molestiae natus quibusdam quis veritatis. Aliquid at eos hic illum obcaecati optio sit velit!</p>
+      <p>Need a developer? <Link to="/contact">Contact Me</Link></p>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,7 @@ const ContactPage = () => {
   return (
     <div>
       <h1>Contact Me</h1>
-      <p>888.888.8888 | me@here.com</p>
+      <p>888.888.8888 | me@here.com | <a href="https://duckduckgo.com" target="_blank">@riadtile</a></p>
     </div>
   )
 }
