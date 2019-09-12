77d0e87c22aa7927664c5816c2695a9b2eadfd55
Joshua Norvelle <joshua.norvelle@gmail.com>
2019-09-11 22:42:47 -0500

    Clean up homepage links

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>I'm Josh, a full-stack developer living in beautiful Denton.</h2>
-      <p>Need a developer? <Link to="/contact">Contact Me</Link></p>
     </Layout>
   )
 }
